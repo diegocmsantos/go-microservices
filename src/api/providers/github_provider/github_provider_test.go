cecf088303013b09ab457b821ed3aa7b1f414c74
Diego Maia <diegocmsantos@gmail.com>
2020-08-17 17:32:12 +0100

    Mocking restclient to be able to test github_provider

--- src/api/providers/github_provider/github_provider_test.go
+++ src/api/providers/github_provider/github_provider_test.go
@@ -1,11 +1,135 @@
 package github_provider
 
 import (
+	"errors"
 	"github.com/stretchr/testify/assert"
+	"go-microservices/src/api/clients/restclient"
+	"go-microservices/src/api/domain/github"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"strings"
 	"testing"
 )
+
+func TestMain(m *testing.M) {
+	restclient.StartMockups()
+	os.Exit(m.Run())
+}
 
 func TestGetAuthorizationHeader(t *testing.T) {
 	header := getAuthorizationHeader("asdf")
 	assert.EqualValues(t, "token asdf", header)
+}
+
+func TestCreateRepoErrorRestClient(t *testing.T) {
+	restclient.FlushMockups()
+	restclient.AddMockup(restclient.Mock{
+		Url:        "https://github.com/user/repos",
+		HttpMethod: http.MethodPost,
+		Err:        errors.New("invalid restclient response"),
+	})
+
+	response, err := CreateRepo("", github.CreateRepoRequestBody{})
+
+	assert.Nil(t, response)
+	assert.NotNil(t, err)
+	assert.EqualValues(t, "invalid restclient response", err.Message)
+}
+
+func TestCreateRepoInvalidResponseBody(t *testing.T) {
+	restclient.FlushMockups()
+	invalidJSONCloser, _ := os.Open("-asf3")
+	restclient.AddMockup(restclient.Mock{
+		Url:        "https://github.com/user/repos",
+		HttpMethod: http.MethodPost,
+		Response: &http.Response{
+			StatusCode: http.StatusCreated,
+			Body:       invalidJSONCloser,
+		},
+	})
+
+	response, err := CreateRepo("", github.CreateRepoRequestBody{})
+
+	assert.Nil(t, response)
+	assert.NotNil(t, err)
+	assert.EqualValues(t, http.StatusInternalServerError, err.StatusCode)
+	assert.EqualValues(t, "invalid response body", err.Message)
+}
+
+func TestCreateRepoInvalidInterface(t *testing.T) {
+	restclient.FlushMockups()
+	restclient.AddMockup(restclient.Mock{
+		Url:        "https://github.com/user/repos",
+		HttpMethod: http.MethodPost,
+		Response: &http.Response{
+			StatusCode: http.StatusUnauthorized,
+			Body:       ioutil.NopCloser(strings.NewReader(`{"message": 1}`)),
+		},
+	})
+
+	response, err := CreateRepo("", github.CreateRepoRequestBody{})
+
+	assert.Nil(t, response)
+	assert.NotNil(t, err)
+	assert.EqualValues(t, http.StatusInternalServerError, err.StatusCode)
+	assert.EqualValues(t, "invalid json response body", err.Message)
+}
+
+func TestCreateRepoUnauthorized(t *testing.T) {
+	restclient.FlushMockups()
+	restclient.AddMockup(restclient.Mock{
+		Url:        "https://github.com/user/repos",
+		HttpMethod: http.MethodPost,
+		Response: &http.Response{
+			StatusCode: http.StatusUnauthorized,
+			Body:       ioutil.NopCloser(strings.NewReader(`{"message": "Requires Authentication", "documentation_url": "any string"}`)),
+		},
+	})
+
+	response, err := CreateRepo("", github.CreateRepoRequestBody{})
+
+	assert.Nil(t, response)
+	assert.NotNil(t, err)
+	assert.EqualValues(t, http.StatusUnauthorized, err.StatusCode)
+	assert.EqualValues(t, "Requires Authentication", err.Message)
+}
+
+func TestCreateRepoInvalidSuccessResponse(t *testing.T) {
+	restclient.FlushMockups()
+	restclient.AddMockup(restclient.Mock{
+		Url:        "https://github.com/user/repos",
+		HttpMethod: http.MethodPost,
+		Response: &http.Response{
+			StatusCode: http.StatusCreated,
+			Body:       ioutil.NopCloser(strings.NewReader(`{"id": "1"}`)),
+		},
+	})
+
+	response, err := CreateRepo("", github.CreateRepoRequestBody{})
+
+	assert.Nil(t, response)
+	assert.NotNil(t, err)
+	assert.EqualValues(t, http.StatusInternalServerError, err.StatusCode)
+	assert.EqualValues(t, "error trying to unmarshal successfully create repository github response", err.Message)
+}
+
+func TestCreateRepoNoError(t *testing.T) {
+	restclient.FlushMockups()
+	restclient.AddMockup(restclient.Mock{
+		Url:        "https://github.com/user/repos",
+		HttpMethod: http.MethodPost,
+		Response: &http.Response{
+			StatusCode: http.StatusCreated,
+			Body:       ioutil.NopCloser(strings.NewReader(`{"id": 1, "name":"test-golang-create-repo", "full_name": "diegocmsantos/test-golang-create-repo"}`)),
+		},
+	})
+
+	response, err := CreateRepo("", github.CreateRepoRequestBody{})
+
+	assert.Nil(t, err)
+	assert.NotNil(t, response)
+	assert.EqualValues(t, 1, response.Id)
+	assert.EqualValues(t, "test-golang-create-repo", response.Name)
+	assert.EqualValues(t, "diegocmsantos/test-golang-create-repo", response.FullName)
 }
